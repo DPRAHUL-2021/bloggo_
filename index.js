26484d3832ee2742c18d67ec5ed25cd710b8cd09
DPRAHUL <dpranavr2021@gmail.com>
2025-01-18 16:28:24 +0530

    Blog Model and blog pages created

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const userRouter = require('./routes/user');
+
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+
+const userRouter = require('./routes/user');
+const blogRouter = require('./routes/blog');
+
+const Blog = require('./models/Blog');
 
 
 const app = express();
@@ -21,12 +26,16 @@ app.set('views',path.resolve("./views"))
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 app.use("/user",userRouter);
+app.use("/blog",blogRouter);
 
-app.get("/",(req,res)=>{
+app.get("/",async (req,res)=>{
+    const allBlogs = await Blog.find({});
     res.render("home",{
       user:req.user,
+      blogs:allBlogs,
     });
 })
 
